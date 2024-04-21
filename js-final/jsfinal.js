edfa1d70415080b4f417321a386bf3567ae776b9
jeanbuley <kyrabuley3@gmail.com>
2024-04-20 23:28:39 -0600

    rough

---
+++ js-final/jsfinal.js
@@ -0,0 +1,52 @@
+volume = 0;
+generated = 0;
+
+displayVolume = "Volume: " + volume;
+displayGenerated = "Generated Number: " + generated;
+
+const btnG = document.querySelector('#generate');
+btnG.addEventListener('click', generateNumber);
+
+const btnA = document.querySelector('#add');
+btnA.addEventListener('click', addNumber);
+
+const btnR = document.querySelector('#reset');
+btnR.addEventListener('click', resetNumber);
+
+function generateNumber(){
+    generated = Math.floor(Math.random() * 101);
+    displayGenerated = "Generated Number: " + generated;
+    console.log(generated);
+    console.log(volume);
+    display();
+}
+
+function addNumber(){
+    volume += generated;
+    generated = 0;
+    displayVolume = "Volume: " + volume;
+    displayGenerated = "Generated Number: " + generated;
+    console.log(generated);
+    console.log(volume);
+    display();
+}
+
+function resetNumber(){
+    volume = 0;
+    generated = 0;
+    displayVolume = "Volume: " + volume;
+    displayGenerated = "Generated Number: " + generated;
+    console.log(generated);
+    console.log(volume);
+    display();
+}
+
+function display(){
+    const volumeText = document.querySelector('#total');
+    volumeText.textContent = displayVolume;
+
+    const generatedText = document.querySelector('#generated');
+    generatedText.textContent = displayGenerated;
+}
+
+
